cb9e86aa51533022c9aca003d8a47fb370abd8fa
Andres Marquez <72410509+andresgmg@users.noreply.github.com>
2023-11-27 15:40:29 -0300

    my first commit

---
+++ rate-repository-app/App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Mi primera aplicacion en Mobile!</Text>
+      <Button
+        title="Press me"
+        onPress={() => Alert.alert('Presionaste el boton!')}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
